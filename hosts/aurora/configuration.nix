7772443c4a091517f17557aa86fa925627a29f6c
Ghil <glabrie85@gmail.com>
2024-03-16 20:48:12 -0400

    oopsie

--- hosts/aurora/configuration.nix
+++ hosts/aurora/configuration.nix
@@ -106,8 +106,8 @@
 #  programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
 #
 #  services.desktopManager.plasma6.enable = true;
-  services.desktopManager.gnome.enable = true;
-  services.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
 
 # Keymap
   services.xserver.xkb = {
